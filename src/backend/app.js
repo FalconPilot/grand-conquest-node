0aac45eff43664bc6e958db221f0067168e8e7d2
Raphael Pallies <raphael@permettezmoideconstruire.fr>
2019-01-28 17:03:00 +0100

    updated app router for basic testing

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,11 @@
 const app = require('express')()
 
-const Format = require('./utils/format.js')
+const { arrow } = require('./utils/format.js')
+
+// Start application router
+require('./router')(app)
 
 // Start server
 app.listen(process.env.CONFIG_PORT, () => {
-  console.log(`${Format.arrow()} Server is now listening on port ${process.env.CONFIG_PORT}`)
+  console.log(`${arrow()} Server is now listening on port ${process.env.CONFIG_PORT}`)
 })
